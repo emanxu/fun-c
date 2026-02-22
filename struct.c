14affa04a564cf7a923f99872a74d64033749951
emanxu <77891264+emanxu@users.noreply.github.com>
2026-02-22 23:52:31 +0800

    add qsort for Struct

--- struct.c
+++ struct.c
@@ -15,6 +15,22 @@ void printPoint(struct PointWithFunction *p) {
   printf("Point: (%d, %d)\n", p->x, p->y);
 }
 
+// use qsort to sort an array of structures based on a member variable
+// Person structure with name and age
+typedef struct {
+  char name[50];
+  int age;
+} Person;
+
+// Comparison function for qsort to sort by age
+// this function will be passed to qsort and will be called for each pair of
+// elements to determine their order
+int compareByAge(const void *a, const void *b) {
+  Person *personA = (Person *)a;
+  Person *personB = (Person *)b;
+  return personA->age - personB->age; // Sort in ascending order
+}
+
 int main() {
   struct Point p1 = {10, 20};
   printf("Point p1: (%d, %d)\n", p1.x, p1.y);
@@ -27,6 +43,26 @@ int main() {
   struct PointWithFunction p2 = {50, 60, printPoint};
   // Calling the function pointer to print the point
   p2.print(&p2);
+
+  printf("\n");
+
+  // Array of Person structures
+  Person people[3] = {{"Alice", 30}, {"Bob", 25}, {"Charlie", 35}};
+
+  // printing unsorted array
+  printf("Unsorted People:\n");
+  for (int i = 0; i < 3; i++) {
+    printf("%s: %d\n", people[i].name, people[i].age);
+  }
+
+  // Sorting the array of Person structures by age using qsort
+  qsort(people, 3, sizeof(Person), compareByAge);
+
+  // printing sorted array
+  printf("\nSorted People by Age:\n");
+  for (int i = 0; i < 3; i++) {
+    printf("%s: %d\n", people[i].name, people[i].age);
+  }
 
   return 0;
 }
